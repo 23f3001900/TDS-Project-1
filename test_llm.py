9b0892e1f7ae9356c13831afa31b21ae30fc251b
23f3001900 <23f3001900@ds.study.iitm.ac.in>
2025-10-17 22:21:08 +0530

    Delete test_llm.py

--- test_llm.py
+++
@@ -1,5 +0,0 @@
-from app.llm_generator import generate_app_code
-
-files = generate_app_code("Simple calculator with buttons 0-9 and + - * / operations", [])
-print(files.get("index.html", "")[:500])
-print(files.get("README.md", ""))
